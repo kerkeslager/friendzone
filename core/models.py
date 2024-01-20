7cd9e5e4547f7326f7a128aaca4b2c6c4fc56de1
David Kerkeslager <david@kerkeslager.com>
2024-01-19 22:10:48 -0500

    Sketch out model structure

--- core/models.py
+++ core/models.py
@@ -1,4 +1,46 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 class User(AbstractUser):
-    pass
+    name = models.CharField(max_length=256)
+
+class Invitation(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='invitations'
+    )
+    circles = models.ManyToManyField(
+        'Circle',
+        related_name='+',
+    )
+
+class Connection(models.Model):
+    inviting_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='+'
+    )
+    accepting_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='+'
+    )
+
+    circles_owned_by_inviting_user = models.ManyToManyField(
+        'Circle',
+        related_name='+',
+    )
+    circles_owned_by_accepting_user = models.ManyToManyField(
+        'Circle',
+        related_name='+',
+    )
+
+class Circle(models.Model):
+    name = models.CharField(max_length=64)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='circles'
+    )
