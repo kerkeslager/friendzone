2f657dc17939fbbfd204cb5959df4325706f40db
David Kerkeslager <david@kerkeslager.com>
2024-01-21 01:00:32 -0500

    Login the user on signup

--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, get_user_model, login
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
@@ -13,10 +14,26 @@ class SettingsView(TemplateView):
 
 settings = SettingsView.as_view()
 
+class SignupForm(UserCreationForm):
+    class Meta:
+        model = get_user_model()
+        fields = UserCreationForm.Meta.fields
+
 class SignupView(CreateView):
-    form_class = UserCreationForm
+    form_class = SignupForm
     success_url = reverse_lazy('welcome') # TODO Redirect to a welcome page
     template_name = 'registration/signup.html'
+
+    def form_valid(self, form):
+        result = super().form_valid(form)
+
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password1']
+
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+
+        return result
 
 signup = SignupView.as_view()
 
