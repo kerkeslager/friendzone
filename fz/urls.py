736142f41d82264fae0ef9aa8ac9664612f5c02b
David Kerkeslager <david@kerkeslager.com>
2024-01-20 21:46:05 -0500

    Allow login and logout

--- fz/urls.py
+++ fz/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import include, path
+
+auth_urlpatterns = [
+    path('login', auth_views.LoginView.as_view(), name='login'),
+    path('logout', auth_views.LogoutView.as_view(), name='logout'),
+]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include(auth_urlpatterns)),
     path('', include('core.urls')),
 ]
