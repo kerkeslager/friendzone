f50500dd34641d2c34268b531302792c55d5fe43
David Kerkeslager <david@kerkeslager.com>
2024-01-21 00:33:04 -0500

    Add signup and welcome pages

--- core/views.py
+++ core/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 from django.views.generic.base import TemplateView
 
 class IndexView(TemplateView):
@@ -9,3 +12,15 @@ class SettingsView(TemplateView):
     template_name = 'core/settings.html'
 
 settings = SettingsView.as_view()
+
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('welcome') # TODO Redirect to a welcome page
+    template_name = 'registration/signup.html'
+
+signup = SignupView.as_view()
+
+class WelcomeView(TemplateView):
+    template_name = 'core/welcome.html'
+
+welcome = WelcomeView.as_view()
--- fz/urls.py
+++ fz/urls.py
@@ -18,11 +18,14 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
+from core import views as core_views
+
 auth_urlpatterns = [
     path('login', auth_views.LoginView.as_view(), name='login'),
     path('logout', auth_views.LogoutView.as_view(), name='logout'),
     path('password-change', auth_views.PasswordChangeView.as_view(), name='password_change'),
     path('password-change-done', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('signup', core_views.signup, name='signup'),
 ]
 
 urlpatterns = [
