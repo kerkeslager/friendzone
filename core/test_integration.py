f64f11ea0c4872765e7091e8f384f5eea057a787
Nikou Zarrabi <zarrabinikou@gmail.com>
2024-02-02 08:15:21 -0500

    Double check user signup
    I might have made some poor assumptions last commits.

--- core/test_integration.py
+++ core/test_integration.py
@@ -29,31 +29,6 @@ class IntegrationTests(StaticLiveServerTestCase):
             cls.browser.quit()
         super().tearDownClass()
 
-    def test_signup_success(self):
-        # Navigate to the signup page
-        self.home_url = self.live_server_url + reverse('welcome')  # 'home' is the name of your post-login redirect URL
-        self.signup_url = self.live_server_url + reverse('signup')  # 'signup' is the name of your signup URL
-        self.browser.get(self.signup_url)
-        # Fill in the username and password fields
-        username_input = self.browser.find_element(By.NAME, 'username')
-        password1_input = self.browser.find_element(By.NAME, 'password1')
-        password2_input = self.browser.find_element(By.NAME, 'password2')
-
-        username_input.send_keys('newuser')
-        password1_input.send_keys('newpassword12')
-        password2_input.send_keys('newpassword12')
-
-        # Submit the form
-        submit_button = self.browser.find_element(By.XPATH, '//button[@type="submit"]')
-        submit_button.click()
-
-        # Wait until the home page is loaded
-        self.wait.until(EC.url_to_be(self.home_url))
-
-        # Assert that the browser redirects to the home page
-        self.assertEqual(self.browser.current_url, self.home_url)
-
-
 
     def test_login_success(self):
         # Navigate to the login page
@@ -80,18 +55,68 @@ class IntegrationTests(StaticLiveServerTestCase):
         # Assert that the browser redirects to the home page
         self.assertEqual(self.browser.current_url, self.home_url)
 
-class TestUserSignupChrome(IntegrationTests):
-    @classmethod
-    def setUpClass(cls):
-        cls.browser = webdriver.Chrome()
-        super().setUpClass()
 
 
-class TestUserSignupFirefox(IntegrationTests):
-    @classmethod
-    def setUpClass(cls):
-        cls.browser = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
-        super().setUpClass()
+    ## With signing up, if I set up the signup_url to be reverse('signup'), I'm currently going from the 
+    # Login test success logged in as testuser. auth/login takes me to to home_url which is index.    
+    # I'm not sure if I should be testing the signup page in the same test as the login page.
+    # Well, I'm accessing the signup page from the login page going from auth/login to auth/signup... Seems like bad practice 
+    # Check below for how we might check for a failed signup.
+    def test_signup_success(self):
+        # Navigate to the signup page
+        self.home_url = self.live_server_url + reverse('welcome')  # 'home' is the name of your post-login redirect URL
+        self.signup_url = self.live_server_url + reverse('signup')  # 'signup' is the name of your signup URL
+        self.browser.get(self.signup_url)
+        # Fill in the username and password fields
+        username_input = self.browser.find_element(By.NAME, 'username')
+        password1_input = self.browser.find_element(By.NAME, 'password1')
+        password2_input = self.browser.find_element(By.NAME, 'password2')
+
+        username_input.send_keys('newuser')
+        password1_input.send_keys('newpassword12')
+        password2_input.send_keys('newpassword12')
+
+        # Submit the form
+        submit_button = self.browser.find_element(By.XPATH, '//button[@type="submit"]')
+        submit_button.click()
+
+        # Wait until the home page is loaded
+        self.wait.until(EC.url_to_be(self.home_url))
+
+        # Assert that the browser redirects to the home page
+        self.assertEqual(self.browser.current_url, self.home_url)
+
+
+
+    # The exceptions are currently text inputs that I might have to search for in the browser such as:
+    # user with this username already exists
+    # how might we deal with this?
+    '''
+    def test_signup_failure(self):
+        # Navigate to the signup page
+        self.home_url = self.live_server_url + reverse('welcome')  # 'home' is the name of your post-login redirect URL
+        self.signup_url = self.live_server_url + reverse('signup')  # 'signup' is the name of your signup URL
+        self.browser.get(self.signup_url)
+        # Fill in the username and password fields
+        username_input = self.browser.find_element(By.NAME, 'username')
+        password1_input = self.browser.find_element(By.NAME, 'password1')
+        password2_input = self.browser.find_element(By.NAME, 'password2')
+
+        username_input.send_keys('testuser')
+        password1_input.send_keys('testpassword')
+        password2_input.send_keys('testpassword')
+
+        # Submit the form
+        submit_button = self.browser.find_element(By.XPATH, '//button[@type="submit"]')
+        submit_button.click()
+
+        # Wait until the home page is loaded
+        self.wait.until(EC.url_to_be(self.home_url))
+
+        # Assert that the browser redirects to the home page
+        self.assertEqual(self.browser.current_url, self.home_url)
+    '''
+
 
 class TestUserLoginChrome(IntegrationTests):
     @classmethod
