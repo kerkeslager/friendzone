e13b74c2be07149b82d8980bcd3ebf36222ab127
David Kerkeslager <david@kerkeslager.com>
2024-02-05 09:51:40 -0500

    Render markdown using markdown-it

--- core/templatetags/markdown.py
+++ core/templatetags/markdown.py
@@ -2,9 +2,15 @@ from django import template
 from django.template.defaultfilters import stringfilter
 from django.utils.safestring import mark_safe
 
+import markdown_it
+
 register = template.Library()
 
 @register.filter(name='markdown')
 @stringfilter
 def render_markdown(source):
-    return mark_safe(f"<section class='markdown'>{ source }</section>")
+    renderer = markdown_it.MarkdownIt('js-default')
+    result = renderer.render(source)
+    result = "<section class='markdown'>{}</section>".format(result)
+    result = mark_safe(result)
+    return result
