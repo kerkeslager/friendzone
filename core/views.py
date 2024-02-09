21acfeae7e69228fdc8272524ecfb5c829260767
David Kerkeslager <david@kerkeslager.com>
2024-02-09 18:59:26 -0500

    Fix bugs in inclusion of feeds for user (#37)

--- core/views.py
+++ core/views.py
@@ -310,7 +310,7 @@ class UserDetailView(DetailView):
                 connections__other_user=result['object']
             )
 
-        result['feed_for_user'] = self.request.feed_for_user(result['object'])
+        result['feed_for_user'] = self.request.user.feed_for_user(result['object'])
 
         return result
 
