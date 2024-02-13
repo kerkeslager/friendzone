ea7ce276a642532d643df6acc14120ff12ae6c14
David Kerkeslager <david@kerkeslager.com>
2024-02-13 12:21:50 -0500

    Write tests against image cropping

--- core/images.py
+++ core/images.py
@@ -11,32 +11,32 @@ class ImageCrop(namedtuple('ImageCrop', ('image_width', 'image_height', 'x0', 'x
 
     @property
     def apply_left(self):
-        return -100 * self.x0 // self.crop_width
+        return -100 * self.x0 / self.crop_width
 
     @property
     def apply_top(self):
-        return -100 * self.y0 // self.crop_height
+        return -100 * self.y0 / self.crop_height
 
     @property
     def apply_width(self):
-        return 100 * self.image_width // self.crop_width
+        return 100 * self.image_width / self.crop_width
 
     @property
     def apply_height(self):
-        return 100 * self.image_height // self.crop_height
+        return 100 * self.image_height / self.crop_height
 
     @property
     def show_left(self):
-        return 100 * self.x0 // self.image_width
+        return 100 * self.x0 / self.image_width
 
     @property
     def show_top(self):
-        return 100 * self.y0 // self.image_height
+        return 100 * self.y0 / self.image_height
 
     @property
     def show_width(self):
-        return 100 * self.crop_width // self.image_width
+        return 100 * self.crop_width / self.image_width
 
     @property
     def show_height(self):
-        return 100 * self.crop_height // self.image_height
+        return 100 * self.crop_height / self.image_height
