9427410335b3f8282d38eb3e7ac85f90ae9998f2
David Kerkeslager <david@kerkeslager.com>
2024-02-13 11:11:31 -0500

    Remove unecessary redirection properties

--- core/images.py
+++ core/images.py
@@ -2,14 +2,6 @@ from collections import namedtuple
 
 class ImageCrop(namedtuple('ImageCrop', ('original_width', 'original_height', 'x0', 'x1', 'y0', 'y1'))):
     @property
-    def left(self):
-        return self.x0
-
-    @property
-    def top(self):
-        return self.y0
-
-    @property
     def width(self):
         return self.x1 - self.x0
 
@@ -19,11 +11,11 @@ class ImageCrop(namedtuple('ImageCrop', ('original_width', 'original_height', 'x
 
     @property
     def apply_left(self):
-        return -100 * self.left // self.width
+        return -100 * self.x0 // self.width
 
     @property
     def apply_top(self):
-        return -100 * self.top // self.height
+        return -100 * self.y0 // self.height
 
     @property
     def apply_width(self):
@@ -35,11 +27,11 @@ class ImageCrop(namedtuple('ImageCrop', ('original_width', 'original_height', 'x
 
     @property
     def show_left(self):
-        return 100 * self.left // self.original_width
+        return 100 * self.x0 // self.original_width
 
     @property
     def show_top(self):
-        return 100 * self.top // self.original_height
+        return 100 * self.y0 // self.original_height
 
     @property
     def show_width(self):
