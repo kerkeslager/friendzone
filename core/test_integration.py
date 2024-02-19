1d11f2d38967fe33fabf3fac1d8872a9e30ce855
Nikou Zarrabi <zarrabinikou@gmail.com>
2024-02-19 11:25:19 -0500

    Revert "try eager load style for chrome"
    This reverts commit d1aa6865985ad064736b70f3a3fe7a3a999a633a.

--- core/test_integration.py
+++ core/test_integration.py
@@ -227,7 +227,7 @@ class ChromeIntegrationTests(IntegrationTests, StaticLiveServerTestCase):
     def setUpClass(cls):
         options = ChromeOptions()
 
-        options.page_load_strategy = 'eager'
+        options.page_load_strategy = 'normal'
 
         if settings.TEST_INTEGRATION_HEADLESS:
             options.add_argument('--headless=new')
