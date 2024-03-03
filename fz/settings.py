4b62743c8d7aced29a1d89af06a7fb89359ee2f9
Nikou Zarrabi <zarrabinikou@gmail.com>
2024-03-03 15:33:44 -0500

    y
    and now?

--- fz/settings.py
+++ fz/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,10 +88,7 @@ WSGI_APPLICATION = 'fz.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 LOGIN_REDIRECT_URL = 'index'
